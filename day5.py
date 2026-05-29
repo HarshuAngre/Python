2c073789b23cbf1ba3df8476136bdf9594ade2fc
Harshu Angre <harshuangre52@gmail.com>
2026-05-30 03:21:25 +0530

    day 5 completed

---
+++ day5.py
@@ -0,0 +1,287 @@
+"""lst = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']
+print(lst[0])  
+print(lst[3])  
+print(lst[-1])  
+
+mixed_data_types = ['Harshu', 20, 5.8, 'Pune', 'not_married']
+print(mixed_data_types)
+
+companies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon' ]
+print(companies)
+print(len(companies))
+
+print(companies[0])
+print(companies[3])
+print(companies[-1])"""
+
+"""companies[0] = 'Meta'
+print(companies)
+companies.append('Twitter')
+print(companies)
+companies.insert(3, 'Tesla')    
+print(companies)
+
+print(companies[1].upper())
+
+result = '#: '.join(companies)
+print(result)"""
+
+"""print('Facebook' in companies)
+companies.sort()
+print(companies)
+print(companies.reverse())
+print(companies)
+
+print(companies[0:3])
+print(companies[-1:-4:-1])
+print(companies[2:5])
+
+companies.remove('Facebook')
+print(companies)
+companies.clear()
+print(companies)
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node', 'Express', 'MongoDB']
+full_stack = front_end + back_end
+print(full_stack)
+full_stack.insert(5, 'Python')
+full_stack.insert(6, 'SQL')
+print(full_stack)"""
+
+
+"""ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages)"""
+
+"""min = min(ages)
+max = max(ages)
+print('Min age:', min)
+print('Max age:', max)
+
+ages.append(min)
+ages.append(max)
+print(ages)
+average = sum(ages) / len(ages)
+print('Median age:', ages[len(ages) // 2])
+print('average : ', sum(ages) / len(ages))
+
+print('Range of ages:', max(ages) - min(ages))
+
+print(abs(min(ages) - average))
+print(abs(max(ages) - average))"""
+
+""""countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+];
+
+middle = len(countries) // 2
+print(countries[middle])
+
+half = len(countries) // 2 + 1
+print(countries[:half])
+print(countries[half:])
+print(len(countries[half:]))
+print(len(countries[:half]))
+
+first = countries[0]
+sencond = countries[1]
+third = countries[2]
+scandic = countries[3:]
+
+print(first, sencond, third)
+print(scandic)"""
