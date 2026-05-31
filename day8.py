a6d3a82e551235d7e3a9fe1b22736983fd48a712
Harshu Angre <harshuangre52@gmail.com>
2026-06-01 03:37:53 +0530

    day 8 completed

---
+++ day8.py
@@ -0,0 +1,29 @@
+dog = {'name': 'max', 'color' : 'black', 'breed': 'labrador', 'age': 5}
+
+"""student = {'first_name': 'harshu',
+        'last_name': 'angre', 
+       'gender': 'male',
+        'age': 20, 
+        'martial_status': 'single',
+          'skills': ['python', 'java', 'c++', 'javascript'],
+          'country': 'india',
+          'address': {'street': '123 main st',
+                      'city': 'mumbai'
+        }
+          }
+
+print(len(student))
+print(type(student['skills']))
+student['skills'].append('html')
+student['skills'].append('css')
+print(student['skills'])
+
+keys = student.keys()
+print(keys)
+values =student.values()
+print(values)
+print(student.items())
+
+student.pop('first_name')
+print(student)"""
+del dog
