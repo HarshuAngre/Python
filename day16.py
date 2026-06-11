9d80778ef8a499b8695687b72d4bfddf111eb88c
Harshu Angre <harshuangre52@gmail.com>
2026-06-12 02:49:36 +0530

    day 16

---
+++ day16.py
@@ -0,0 +1,34 @@
+"""from datetime import datetime,date
+
+now = datetime.now()
+
+day = now.day
+month = now.month
+year = now.year
+hour = now.hour
+minute = now.minute
+timestamp = now.timestamp()
+
+print(day,month,year,hour,minute)
+print(timestamp)
+
+t = now.strftime("%m/%d/%Y , %H:%M:%S")
+
+print(t)
+
+date_str = "5 December, 2019"
+print(date_str)
+
+date_obj = datetime.strptime(date_str, "%d %B, %Y")
+print(date_obj)
+
+
+today = date(year=2026,month=6,day=12)
+new_year = date(year=2027,month=1,day=1)
+
+time_left = new_year - today
+
+print(time_left)
+print(now.timestamp())"""
+
+
