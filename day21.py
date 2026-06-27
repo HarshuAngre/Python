bf7d6ff06e9a966043afd796d9f04c14c8040eff
Harshu Angre <harshuangre52@gmail.com>
2026-06-28 01:07:22 +0530

    day 21 competed

--- day21.py
+++ day21.py
@@ -83,3 +83,119 @@ person.add_expenses("Food", 6000)
 person.add_expenses("Internet", 1000)
 
 person.account_info()"""
+
+
+
+
+"""
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "http://www.bu.edu/president/boston-university-facts-stats/"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
+
+data = {}
+
+for section in soup.find_all(["section","div"]):
+    heading = section.find(["h2","h3"])
+    if heading:
+        stats = {}
+
+        for items in section.find_all("li"):
+            text = items.get_text(" ", strip=True)
+            if text:
+                stats[f"item_{len(stats)+1}"] = text
+        
+        if stats:
+            data[heading.get_text(strip=True)] = stats
+
+
+with open("bu_facts_stats.json","w",encoding="utf-8") as f:
+    json.dump(data,f,indent=4,ensure_ascii=False)
+
+print("Data successfully saved to bu_facts_stats.json")"""
+
+
+"""import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://archive.ics.uci.edu/ml/datasets.php"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
+
+tables = soup.find_all("tables")
+
+table = tables[5]
+
+headers = []
+
+header_row = table.find("tr")
+for th in header_row.find_all(["th", "td"]):
+    headers.append(th.text(strip=True))
+
+data = []
+
+for row in table.find_all("tr")[1:]:
+    cols = row.find_all("td")
+
+    if len(row) == len(headers):
+        row_data = {}
+
+        for i in range(len(headers)):
+            row_data[headers[i]] = cols[i].get_text(" ", strip=True)
+        data.append(row_data)
+
+with open("uci_datasets.json", "w", encoding="utf-8") as file:
+    json.dump(data,file,indent=4)
+
+print("Data saved to uci_datasets.json")"""
+
+
+"""import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
+
+# Get webpage
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find the first wikitable (Presidents table)
+table = soup.find("table", class_="wikitable")
+
+# Get column headers
+headers = []
+header_row = table.find("tr")
+
+for th in header_row.find_all("th"):
+    header = th.get_text(" ", strip=True)
+    headers.append(header)
+
+# Extract rows
+presidents = []
+
+for row in table.find_all("tr")[1:]:
+    cols = row.find_all(["td", "th"])
+
+    # Skip incomplete rows
+    if len(cols) < len(headers):
+        continue
+
+    president = {}
+
+    for i in range(len(headers)):
+        president[headers[i]] = cols[i].get_text(" ", strip=True)
+
+    presidents.append(president)
+
+# Save as JSON
+with open("us_presidents.json", "w", encoding="utf-8") as file:
+    json.dump(presidents, file, indent=4, ensure_ascii=False)
+
+print("Data saved to us_presidents.json")"""
